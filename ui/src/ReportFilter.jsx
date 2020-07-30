0bfd889fed5f84681254466907809cee3ef99e3e
Chris <tso.ch@husky.neu.edu>
2020-07-29 21:05:05 -0400

    update report filter

--- ui/src/ReportFilter.jsx
+++ ui/src/ReportFilter.jsx
@@ -19,12 +19,65 @@ class ReportFilter extends React.Component {
     return (
       <Panel>
         <Panel.Heading>
-          <Panel.Title>This is for filter</Panel.Title>
+          <Panel.Title>Report Filter</Panel.Title>
         </Panel.Heading>
         <Panel.Body>
           <Form horizontal onSubmit={this.handleSubmit}>
-            {/* Add contents here */}
-
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={3}>Year</Col>
+            <Col sm={9}>
+              <FormControl
+                componentClass="select"
+                name="Year"
+                value="Year"
+                onChange={this.onChange}
+              >
+                <option value="2008">2008</option>
+                <option value="2009">2009</option>
+                <option value="2010">2010</option>
+                <option value="2011">2011</option>
+                <option value="2012">2012</option>
+                <option value="2013">2013</option>
+                <option value="2014">2014</option>
+              </FormControl>
+            </Col>
+          </FormGroup>
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={3}>Crime Type</Col>
+            <Col sm={9}>
+              <FormControl
+                componentClass="select" multiple
+                name="Crime Type"
+                value="Crime Type"
+                onChange={this.onChange}
+              >
+                <option value="Assault">Assault</option>
+                <option value="Burglary">Burglary</option>
+                <option value="Homicide">Homicide</option>
+                <option value="Larceny-Theft">Larceny-Theft</option>
+                <option value="Motor Vehicle Theft">Motor Vehicle Theft</option>
+                <option value="Rape">Rape</option>
+                <option value="Robbery">Robbery</option>
+              </FormControl>
+            </Col>
+          </FormGroup>
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={3}>Precint</Col>
+            <Col sm={9}>
+              <FormControl
+                componentClass="select" multiple
+                name="Precint"
+                value="Precint"
+                onChange={this.onChange}
+              >
+                <option value="E">E</option>
+                <option value="N">N</option>
+                <option value="SE">S</option>
+                <option value="SW">SW</option>
+                <option value="W">W</option>
+              </FormControl>
+            </Col>
+          </FormGroup>
             <ButtonToolbar>
               <Button
                 disabled={false}
@@ -37,7 +90,6 @@ class ReportFilter extends React.Component {
           </Form>
         </Panel.Body>
         <Panel.Footer>
-          This is the Panel.Footer
         </Panel.Footer>
       </Panel>
     );
