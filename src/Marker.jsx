6f8dfa8b7346fdd13080d2f0ce68554c085d7008
boniu <niu.bo@husky.neu.edu>
2020-08-03 16:41:38 -0700

    add marker callback

--- src/Marker.jsx
+++ src/Marker.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
-const Marker = (props) => {
-  const { color, name, id } = props;
-  return (
-    <div
-      className="marker"
-      style={{ backgroundColor: color, cursor: 'pointer' }}
-      title={name}
-    />
-  );
-};
+class Marker extends React.Component {
+  constructor() {
+    super();
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    console.log('marker clicked');
+  }
+
+  render() {
+    const { color, name, id } = this.props;
+    return (
+      <div
+        className="marker"
+        style={{ backgroundColor: color, cursor: 'pointer' }}
+        title={name}
+        role="button"
+        onClick={this.onClick}
+        onKeyPress={onKeyPressHandler}
+      />
+    );
+  }
+}
 
 export default Marker;
