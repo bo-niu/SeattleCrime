b7a98a6ec2c649a052406502605b4a04fdade065
boniu <niu.bo@husky.neu.edu>
2020-08-06 06:24:48 -0700

    try to solve marker not showing issue 3: remove NotFound Page

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import Discussion from './Discussion.jsx';
 import Report from './Report.jsx';
 import Home from './Home.jsx';
 import About from './About.jsx';
-import NotFound from './NotFound.jsx';
+// import NotFound from './NotFound.jsx';
 
 const routes = [
   { path: '/discussion', component: Discussion },
   { path: '/home/:id?', component: Home },
   { path: '/report', component: Report },
   { path: '/about', component: About },
-  { path: '*', component: NotFound },
+  // { path: '*', component: NotFound },
 ];
 
 export default routes;
