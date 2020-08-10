ca4df78a0730ec2440f82397e955dc30a085cced
yfwang <wang.yifei6@husky.neu.edu>
2020-08-09 20:27:33 -0700

    Resolve Chrome warning in report

--- src/ReportFilter.jsx
+++ src/ReportFilter.jsx
@@ -46,17 +46,17 @@ class ReportFilter extends React.Component {
                 value={crime}
                 onChange={this.onCrimeChange}
               >
-                <option value="Assault">Assault</option>
-                <option value="Bias Incident">Bias Incident</option>
-                <option value="Disorderly Conduct">Disorderly Conduct</option>
-                <option value="DUI">DUI</option>
-                <option value="Burglary">Burglary</option>
-                <option value="Homicide">Homicide</option>
-                <option value="Injury">Injury</option>
-                <option value="Liquor Violation">Liquor Violation</option>
-                <option value="Narcotics">Narcotics</option>
-                <option value="Pickpocket">Pickpocket</option>
-                <option value="Robbery">Robbery</option>
+                <option key={1} value="Assault">Assault</option>
+                <option key={2} value="Bias Incident">Bias Incident</option>
+                <option key={3} value="Disorderly Conduct">Disorderly Conduct</option>
+                <option key={4} value="DUI">DUI</option>
+                <option key={5} value="Burglary">Burglary</option>
+                <option key={6} value="Homicide">Homicide</option>
+                <option key={7} value="Injury">Injury</option>
+                <option key={8} value="Liquor Violation">Liquor Violation</option>
+                <option key={9} value="Narcotics">Narcotics</option>
+                <option key={10} value="Pickpocket">Pickpocket</option>
+                <option key={11} value="Robbery">Robbery</option>
               </FormControl>
             </Col>
           </FormGroup>
@@ -69,23 +69,23 @@ class ReportFilter extends React.Component {
                 value={district}
                 onChange={this.onDistrictChange}
               >
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="E">E</option>
-                <option value="F">F</option>
-                <option value="G">G</option>
-                <option value="J">J</option>
-                <option value="K">K</option>
-                <option value="L">L</option>
-                <option value="M">M</option>
-                <option value="N">N</option>
-                <option value="O">O</option>
-                <option value="Q">Q</option>
-                <option value="R">W</option>
-                <option value="S">S</option>
-                <option value="U">U</option>
-                <option value="W">W</option>
+                <option key="B" value="B">B</option>
+                <option key="C" value="C">C</option>
+                <option key="D" value="D">D</option>
+                <option key="E" value="E">E</option>
+                <option key="F" value="F">F</option>
+                <option key="G" value="G">G</option>
+                <option key="J" value="J">J</option>
+                <option key="K" value="K">K</option>
+                <option key="L" value="L">L</option>
+                <option key="M" value="M">M</option>
+                <option key="N" value="N">N</option>
+                <option key="O" value="O">O</option>
+                <option key="Q" value="Q">Q</option>
+                <option key="R" value="R">W</option>
+                <option key="S" value="S">S</option>
+                <option key="U" value="U">U</option>
+                <option key="W" value="W">W</option>
               </FormControl>
             </Col>
           </FormGroup>
