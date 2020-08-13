58a563e2ea9dd80488028fde07d2c4de89b63393
yfwang <wang.yifei6@husky.neu.edu>
2020-08-12 18:17:54 -0700

    Add contents in About page

--- src/About.jsx
+++ src/About.jsx
@@ -26,11 +26,27 @@ export default class About extends React.Component {
   render() {
     const { apiAbout } = this.state;
     return (
-      <div className="text-center">
-        <h3>Seattle Crime version 1.0</h3>
-        <h4>
-          {apiAbout}
-        </h4>
+      <div>
+        <div className="text-center">
+          <h3>Seattle crime data visualization project</h3>
+        </div>
+        <h4>Dataset - https://www.kaggle.com/sam/seattle-crime</h4>
+        <h4>For our project, we are proposing to integrate the Seattle Crime dataset from Kaggle with the Google Maps API. This project would be beneficial for Police departments and also for law/policy makers to help determine possible funding allocations.</h4>
+        <h4>Features</h4>
+        <ul class="list-group">
+          <li class="list-group-item">Login page using Google</li>
+          <li class="list-group-item">Visualize crime data on google map</li>
+          <li class="list-group-item">Filter crime by time, type and location on Home page</li>
+          <li class="list-group-item">A visualized chart to show the trend of crimes based on user input on Report page</li>
+          <li class="list-group-item">By hovering on the legends on the map, the detailed crime information will pop up</li>
+          <li class="list-group-item">By clicking the legends on the map, user will be directed to a Discussion page for detailed information and comments of that crime</li>
+          <li class="list-group-item">By typing text and clicking comment button on Discussion page, user can post comments about the crime</li>
+        </ul>
+        <div className="text-center">
+          <h4>
+            {apiAbout}
+          </h4>
+        </div>
       </div>
     );
   }
